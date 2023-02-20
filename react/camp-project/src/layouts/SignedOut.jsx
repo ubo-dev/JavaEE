7b59d0f22b71b029d7f118702e7c3debd0c851f9
Umut Bilal Okur <umutokur11@outlook.com>
2023-02-20 13:48:56 +0300

    sign in-out

---
+++ react/camp-project/src/layouts/SignedOut.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button, Menu } from 'semantic-ui-react'
+
+export default function SignedOut() {
+  return (
+    <div>
+      <Menu.Item>
+        <Button primary>Sign In</Button>
+        <Button primary style = {{marginLeft:"0.5em"}}>Register</Button>
+      </Menu.Item>
+    </div>
+  )
+}
