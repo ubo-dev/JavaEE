10d8af49a09a7a54a478aa08f201bd769b33b6a9
ubo-dev <umutokur11@outlook.com>
2023-07-05 10:30:02 +0300

    maps

--- JavaFunctionalProgramming/com.ubo/src/Main.java
+++ JavaFunctionalProgramming/com.ubo/src/Main.java
@@ -7,8 +7,10 @@ public class Main {
         //printAllNumbersInListFunctional(List.of(12,9,13,4,6,2,4,12,15));
         //printEvenNumbersInListFunctional(List.of(12,9,13,4,6,2,4,12,15));
         //printOddNumbersInListFunctional(List.of(12,9,13,4,6,2,4,12,15));
-        printCoursesContains(List.of("math","algorithm","programming","chemistry","history","biology"),"m");
-
+        //printCoursesContains(List.of("math","algorithm","programming","chemistry","history","biology"),"m");
+        //printSquareOfNumbers(List.of(12,9,13,4,6,2,4,12,15));
+        //printCubesOfNumbers(List.of(12,9,13,4,6,2,4,12,15));
+        printLengthsOfCourses(List.of("math","algorithm","programming","chemistry","history","biology"));
 
     }
 
@@ -43,6 +45,25 @@ public class Main {
     {
         integers.stream()
                 .filter(integer -> integer % 2 != 0)
+                .forEach(System.out::println);
+    }
+
+    private static void printSquareOfNumbers(List<Integer> numbers) {
+        numbers.stream()
+                .map(number -> Math.pow(number,2))
+                .forEach(System.out::println);
+    }
+
+    private static void printCubesOfNumbers(List<Integer> numbers) {
+        numbers.stream()
+                .map(number -> Math.pow(number,3))
+                .forEach(System.out::println);
+    }
+
+    private static void printLengthsOfCourses(List<String> courses)
+    {
+        courses.stream()
+                .map(course -> course + " " + course.length())
                 .forEach(System.out::println);
     }
 
