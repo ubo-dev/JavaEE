3af373fd9bf3b42ac9b2113ae548e4f1acae18bc
ubo-dev <umutokur11@outlook.com>
2023-07-08 12:22:39 +0300

    add user jpa

--- restful-web-services/src/main/java/com/ubo/restfulwebservices/user/UserJpaResource.java
+++ restful-web-services/src/main/java/com/ubo/restfulwebservices/user/UserJpaResource.java
@@ -1,12 +1,14 @@
 package com.ubo.restfulwebservices.user;
 
 import com.ubo.restfulwebservices.jpa.UserRepository;
+import jakarta.validation.Valid;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -43,4 +45,22 @@ public class UserJpaResource
 
         return entityModel;
     }
+
+    @DeleteMapping("/jpa/users/{id}")
+    public void deleteUser(@PathVariable int id)
+    {
+        userRepository.deleteById(id);
+    }
+
+    @PostMapping("/jpa/users")
+    public ResponseEntity<User> createUser(@Valid @RequestBody User user)
+    {
+        User savedUser = userRepository.save(user);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId()).toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+
+
 }
